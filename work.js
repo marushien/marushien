3786f018f24514e91cdf4ecc4aab0ef3a6f62edd
marushien <67651891+marushien@users.noreply.github.com>
2021-07-08 03:08:15 +0800

    Update work.js

--- work.js
+++ work.js
@@ -1,8 +1,8 @@
 let serverVideo;
 let state = "on";
 let broker = {
-  hostname: 'test.mosquitto.org',
-  port: 8080
+  hostname: 'broker.hivemq.com',
+  port: 8000
 };
 let client;
 let topic = 'drop';
